c11c14f23431c003c0d2a57b0cdbd77b23f05a67
MuraliKrishna04 <21PA1A05A7@VISHNU.EDU.IN>
2024-04-21 23:19:36 +0530

    images-upload commit

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const cookieParser = require ('cookie-parser');
 const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
+
 require('dotenv').config();
 const app = express();
 
@@ -14,6 +17,7 @@ const jwtSecret = "nrsbgirfhpir4ir940rwkfiennbdfibmfihlmsejbabdnijdr";
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials:true,
     origin:'http://localhost:5173', // Update this to match the client application's URL
@@ -88,16 +92,33 @@ app.post('/logout',(req,res)=>{
 });
 
 
-console.log({__dirname})
+
 app.post('/upload-by-link',async(req,res)=>{
     const{link} =req.body;
-    const newName = Date.now() + '.jpg';
+    const newName = 'photo-'+ Date.now() + '.jpg';
     await imageDownloader.image({
         url:link,
-        dest: __dirname+'/uploads/'+newName
+        dest: __dirname+ '/uploads/'+newName
     });
-    res.json(__dirname+'/uploads/'+newName);
+    res.json(newName);
+});
+
+
+const photosMiddleware = multer({dest:'uploads/'});
+app.post('/upload',photosMiddleware.array('photos', 100),(req,res)=>{
+    const uploadedFiles = [];
+    for (let i=0; i< req.files.length; i++){
+        const {path,originalname} = req.files[i];
+        const parts = originalname.split('.');
+        const ext = parts[parts.length-1];
+        const newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+        uploadedFiles.push(newPath.replace('uploads/',''));
+    }
+    res.json(uploadedFiles);
 })
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import Perks from "../Perks";
 import axios from "axios";
 
+
 export default function PlacesPage(){
     const {action} =useParams();
     const [title,setTitle] = useState('');
@@ -39,9 +40,28 @@ export default function PlacesPage(){
 
     async function addPhotoByLink(ev){
         ev.preventDefault();
-        await axios.post('/upload-by-link',{link:photoLink})
+        const {data:filename} = await axios.post('/upload-by-link',{link:photoLink})
+        setAddedPhotos(prev =>{
+            return [...prev,filename];
+        });
+        setPhotoLink('');
     }
 
+    function uploadPhoto(ev){
+        const files = ev.target.files;
+        const data = new FormData();
+        for (let i = 0; i < files.length; i++) {
+            data.append('photos',files[i]);
+        }
+        axios.post('/upload',data,{
+            headers:{'Content-type':'multipart/form-data'}
+        }).then(response =>{
+            const {data:filenames} = response;
+            setAddedPhotos(prev =>{
+                return [...prev,...filenames];
+            });
+        })
+    }
 
     return (
         <div>
@@ -75,13 +95,20 @@ export default function PlacesPage(){
                                     placeholder={'Add Using a Link.....jpg'}/>
                             <button onClick={addPhotoByLink} className="bg-gray-200 px-4 rounded-3xl">Add&nbsp;Photos</button>
                         </div>
-                        <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-                            <button className=" flex gap-1.5 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+                        
+                        <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+                            {addedPhotos.length > 0 && addedPhotos.map(link =>(
+                                <div className="h-32 flex">
+                                    <img className='rounded-2xl w-full object-cover position-center'src={'http://localhost:3000/uploads/'+link} alt="photo"/>
+                                </div>
+                            ))}
+                            <label className="h-32 cursor-pointer flex items-center gap-2 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+                                <input type="file" multiple className="hidden" onChange={uploadPhoto}/>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
                                     </svg>
                                 Upload
-                            </button>
+                            </label>
                         </div>
 
 
