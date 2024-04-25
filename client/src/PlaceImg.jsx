0e38ae85ebd4e65d4c2883a691b0342be171cd66
MuraliKrishna04 <21PA1A05A7@VISHNU.EDU.IN>
2024-04-26 00:28:18 +0530

    Bookings-commit

---
+++ client/src/PlaceImg.jsx
@@ -0,0 +1,11 @@
+export default function PlaceImg({place,index=0,className}){
+    if(!place.photos?.length){
+        return '';
+    }
+    if(!className){
+        className = 'object-cover'
+    }
+    return(
+        <img className={className} src={'http://localhost:3000/uploads/'+place.photos[index]}  alt=""/>
+    );
+}
--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import BookingWidget from "../BookingWidget";
+import AddressLink from "../AddressLink";
+import PlaceGallery from "../PlaceGallery";
 
 export default function PlacePage(){
     const {id} =useParams();
@@ -9,19 +12,42 @@ export default function PlacePage(){
         if(!id){
             return;
         }
-        axios.get('/places/${id}').then(response =>{
+        axios.get(`/places/${id}`).then(response =>{
             setPlace(response.data);
         })
     },[id]);
 
-    if(!place){
-        return '';
-    }
+    if(!place) return '';
+    
     return (
-        <div className="mt-8 bg-gray-100 -mx-8 px-8 py-6">
+        <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
             <h1 className="text-3xl">{place.title}</h1>
-            <a className="my-2 block font-semibold underline"target="_blank" href={'https://maps.google.com/q='+place.address}>{place.address}</a>
+            <AddressLink>{place.address}</AddressLink>
+            <PlaceGallery place={place}/>
+        
+            <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
+                <div>
+                    <div className="my-4">
+                        <h2 className="font-semibold text-2xl">Description</h2>
+                        {place.description}
+                    </div>
+                    Check-In:{place.checkIn}<br/>
+                    Check-Out:{place.checkOut}<br/>
+                    Max number of guests:{place.maxGuests}
+                </div>
+                <div>
+                    <BookingWidget place={place}/>
+                </div>
+            </div>
+            <div className="div bg-white -mx-8 px-8 py-8 border-t">
+              <div>
+                <h2 className="font-semibold text-2xl">Extra Info</h2>
+              </div>
+              <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">
+                {place.extraInfo}
+              </div>
+            </div>
             
-        </div>
+        </div>      
     );
 }
